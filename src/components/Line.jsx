db9c647dee3a36368c896b94487d603d0c312387
GeremyColley <78615407+GeremyColley@users.noreply.github.com>
2023-06-29 11:55:44 +0200

    Add line component

---
+++ src/components/Line.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+const Line = ({ projets }) => {
+  const [hoverLine, setHoverLine] = useState(false);
+
+  return (
+    <div
+      // voir la doc sur les "mouse events"
+      // https://fr.reactjs.org/docs/events.html#mouse-events
+      onMouseEnter={() => setHoverLine(true)}
+      onMouseLeave={() => setHoverLine(false)}
+      onClick={() => {
+        navigator.clipboard.writeText(emoji.symbol);
+        // lien vers la solution
+        // Google : react copy to clipboard
+        // https://stackoverflow.com/a/52033479
+      }}
+      className="line"
+    >
+      <span>
+        {projets.NomProjet} {projets.type}
+      </span>
+      {hoverLine && <span className="copy-span">Click to copy !</span>}
+    </div>
+  );
+};
+
+export default Line;
