07ab5345ce02865d122288ce3927e383b8b1ccc8
GeremyColley <78615407+GeremyColley@users.noreply.github.com>
2023-07-05 17:12:01 +0200

    Update css home and publish

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,20 +4,22 @@ import '../assets/css/Home.css'
 const Home = ({data, isLoading}) => {
 
     return isLoading ? ( 
-        <div>
-            <p>Loading...</p>
-        </div>
-        ) : (
-        <div className="home-container">
             <div>
-                <h1>CROWDFUNDING CAMPAIGNS</h1>
-                <p>Nos offres</p>
+                <p>Loading...</p>
             </div>
-            
-            {data}
+        ) : (
+            <div className="home-container">
 
+                <div className="home-titre">
+                    <h1>CROWDFUNDING CAMPAIGNS</h1>
+                    <p>Nos offres</p>
+                </div>
 
-        </div>
+                <div className="line-container">
+                    {data}
+                </div>
+                
+            </div>
     );
 
 }
