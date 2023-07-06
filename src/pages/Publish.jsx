216f5690310b375767fb15021f64d6fabd65101b
GeremyColley <78615407+GeremyColley@users.noreply.github.com>
2023-07-06 09:39:33 +0200

    update

--- src/pages/Publish.jsx
+++ src/pages/Publish.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Link, useNavigate, useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 import '../assets/css/Publish.css'
 
 const Publish = ({token}) => {
